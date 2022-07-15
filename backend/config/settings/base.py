5619b3d0f72fcd87cc86ab536925766c0e985d37
bovage <fayemibolu2@gmail.com>
2022-07-15 07:29:31 -1200

    fix: base dir to be backend/

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
