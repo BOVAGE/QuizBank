d5328beda6f9d6f4d29a1e04e2266f9497457cbb
bovage <fayemibolu2@gmail.com>
2022-07-15 19:03:10 -1200

    build: add permissions - IsAdminOrReadonly in common

---
+++ backend/common/permissions.py
@@ -0,0 +1,11 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+class IsAdminUserOrReadOnly(BasePermission):
+    """
+        give full access to staff user and read-only access
+        to non - staff user
+    """
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        return request.method in SAFE_METHODS
