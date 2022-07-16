03becb2d8bc1dde71584f9ebff340bdc85e06782
bovage <fayemibolu2@gmail.com>
2022-07-15 17:54:38 -1200

    feat: admin full question, verification

--- backend/quiz/urls.py
+++ backend/quiz/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import QuestionListCreateView
+
+from .views import (QuestionListCreateView, QuestionListFullView,
+                    QuestionVerification, UnverifiedQuestionListFullView)
 
 urlpatterns = [
     path('', QuestionListCreateView, name='question-list'),
+    path('full', QuestionListFullView, name='question-list-full'),
+    path('unverified', UnverifiedQuestionListFullView, 
+    name='unverified-question-list-full'),
+    path('<int:id>/verification', QuestionVerification, 
+    name='question-verification'),
 ]
--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,12 +1,14 @@
 from django.contrib.auth import get_user_model
 from django.db.models import Q
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .models import Question
-from .serializers import QuestionPublicSerializer
+from .serializers import QuestionDetailSerializer, QuestionPublicSerializer
 
 User = get_user_model()
 class QuestionListCreateView(generics.GenericAPIView):
@@ -67,4 +69,73 @@ class QuestionListCreateView(generics.GenericAPIView):
         return Response(data, status.HTTP_201_CREATED)
 
 
+class QuestionListFullView(generics.ListAPIView):
+    serializer_class = QuestionDetailSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+    queryset = Question.objects.order_by("-date_created")
+
+    def list(self, request, *args, **kwargs):
+        data = super().list(request, *args, **kwargs).data
+        data = {
+            "status": "success",
+            "message": "All Questions fetched successfully",
+            "data": data
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class UnverifiedQuestionListFullView(generics.ListAPIView):
+    serializer_class = QuestionDetailSerializer
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+    queryset = Question.unverified.order_by("-date_created")
+
+    def list(self, request, *args, **kwargs):
+        data = super().list(request, *args, **kwargs).data
+        data = {
+            "status": "success",
+            "message": "All Unverified Questions fetched successfully",
+            "data": data
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+
+class QuestionVerification(APIView):
+    """
+        verify or unverify the question whose ID was passed in the URL
+    """
+    permission_classes = [IsAdminUser]
+    authentication_classes = [JWTAuthentication]
+
+    def post(self, request, id):
+        """
+            verifies the question whose ID was passed in the URL
+        """
+        question = get_object_or_404(Question, id=id)
+        question.verify(request.user)
+        data = {
+            "status": "success",
+            "message": f"{request.user} verifies question-{question.id}: {question}",
+            "data": []
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    def delete(self, request, id):
+        """
+            unverifies the question whose ID was passed in the URL
+        """
+        question = get_object_or_404(Question, id=id)
+        question.unverify()
+        data = {
+            "status": "success",
+            "message": f"{request.user} unverifies question-{question.id}: {question}",
+            "data": []
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+
 QuestionListCreateView = QuestionListCreateView.as_view()
+QuestionListFullView = QuestionListFullView.as_view()
+UnverifiedQuestionListFullView = UnverifiedQuestionListFullView.as_view()
+QuestionVerification = QuestionVerification.as_view()
