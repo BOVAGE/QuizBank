d5b33e7f0f35bcccef35edc8258507ffbba4b2fe
bovage <fayemibolu2@gmail.com>
2022-07-16 19:04:07 -1200

    build: add writeonly permission to non-staff

--- backend/common/permissions.py
+++ backend/common/permissions.py
@@ -9,3 +9,14 @@ class IsAdminUserOrReadOnly(BasePermission):
         if request.user.is_staff:
             return True
         return request.method in SAFE_METHODS
+
+
+class IsAdminUserorWriteOnly(BasePermission):
+    """
+        give full access to staff user and Write-only access
+        to non - staff user
+    """
+    def has_permission(self, request, view):
+        if request.user.is_staff:
+            return True
+        return request.method == "POST"
