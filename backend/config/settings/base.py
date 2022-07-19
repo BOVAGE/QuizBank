328cf78b3cae31c57d13c51580cb80220a4c6a8c
bovage <fayemibolu2@gmail.com>
2022-07-19 09:20:47 -1200

    build: add cors policy

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -34,10 +34,12 @@ INSTALLED_APPS = [
     
     #third party packages
     'rest_framework',
+    'corsheaders',
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -147,3 +149,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+#CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
